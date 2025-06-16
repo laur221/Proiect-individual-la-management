d6615624c4e98526590557b66b26cb24fe6f1979
laur221 <kridderurt@gmail.com>
2025-06-16 21:55:14 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   preview: {
     port: parseInt(process.env.PORT || '4173'),
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['proiect-individual-la-management.onrender.com']
   }
 })
